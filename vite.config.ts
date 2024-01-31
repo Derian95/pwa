5ea5bf8ae4519d95781948db0a1e7626cbde97f8
derian95 <dfhamezquita@gmail.com>
2024-01-31 11:09:50 -0500

    raa

--- vite.config.ts
+++ vite.config.ts
@@ -10,25 +10,25 @@ const manifest2:VitePWAOptions = {
     short_name: "react-vite-app",
     description: "I am a simple vite app",
     icons: [{
-      src: '/android-chrome-192x192.png',
+      src: './icon-192x192.png',
       sizes: '192x192',
       type: 'image/png',
       purpose: 'favicon'
     },
     {
-      src: '/android-chrome-512x512.png',
+      src: './icon-512x512.png',
       sizes: '512x512',
       type: 'image/png',
       purpose: 'favicon'
     },
     {
-      src: '/apple-touch-icon.png',
+      src: './icon-512x512.png',
       sizes: '180x180',
       type: 'image/png',
       purpose: 'apple touch icon',
     },
     {
-      src: '/maskable_icon.png',
+      src: './icon-512x512.png',
       sizes: '512x512',
       type: 'image/png',
       purpose: 'any maskable',
